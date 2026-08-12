fc66a5e52100314634714f1db1ea565ce750d079
gkgoat <65402989+gkgoat1@users.noreply.github.com>
2026-08-12 06:49:38 -0400

    ert 1.1

--- crates/cirrus-ert/src/lib.rs
+++ crates/cirrus-ert/src/lib.rs
@@ -6,7 +6,7 @@ use cirrus_core::{
     ContextWithAdd, ContextWithBitAnd, ContextWithBitOr, ContextWithBitXor, ContextWithDiv,
     ContextWithMul, ContextWithSub,
 };
-use rv_asm::{DecodeError, Inst};
+use rv_asm::{DecodeError, Inst, Reg};
 pub trait ContextWithRvOps<Val>:
     ContextWithBitAnd<Val> + ContextWithBitOr<Val> + ContextWithBitXor<Val>
 {
@@ -46,11 +46,13 @@ pub fn simple_add<W: Clone, E: Error>(
 pub fn ert_emit<W: Clone, E: Error>(
     t: &mut (dyn ContextWithRvOps<bool, Wrapped = W, Error = E> + '_),
     mem: &mut [u8],
+    vstack: &mut [W],
     mut pc: u32,
     mut regs: [[W; 32]; 32],
     zero: W,
     one: W,
 ) -> Result<[[W; 32]; 32], ErtError<E>> {
+    let mut sp: u32 = 0;
     loop {
         pc &= !3;
         let bc = match rv_asm::Inst::decode(
@@ -60,136 +62,264 @@ pub fn ert_emit<W: Clone, E: Error>(
             Ok((bc, _)) => bc,
             Err(e) => return Err(ErtError::Decode(e)),
         };
+        for r in regs[0].iter_mut() {
+            *r = zero.clone();
+        }
+        for (i, r) in regs[Reg::SP.0 as usize].iter_mut().enumerate() {
+            *r = if sp >> i == 0 {
+                zero.clone()
+            } else {
+                one.clone()
+            }
+        }
         pc = match bc {
+            //arith
             Inst::Addi { imm, dest, src1 } => {
-                if dest.0 != 0 {
-                    let w = array::from_fn(|i| {
-                        if (imm.as_i32() as u32 >> i) & 1 == 1 {
-                            one.clone()
-                        } else {
-                            zero.clone()
-                        }
-                    });
-                    regs[dest.0 as usize] = simple_add(
-                        t,
-                        &regs[src1.0 as usize],
-                        &w,
-                        zero.clone(),
-                        zero.clone(),
-                        one.clone(),
-                    )
-                    .map_err(|e| ErtError::Emitted(e))?;
+                if dest == Reg::SP {
+                    sp = sp.wrapping_add_signed(imm.as_i32());
                 }
+                let w = array::from_fn(|i| {
+                    if (imm.as_i32() as u32 >> i) & 1 == 1 {
+                        one.clone()
+                    } else {
+                        zero.clone()
+                    }
+                });
+                regs[dest.0 as usize] = simple_add(
+                    t,
+                    &regs[src1.0 as usize],
+                    &w,
+                    zero.clone(),
+                    zero.clone(),
+                    one.clone(),
+                )
+                .map_err(|e| ErtError::Emitted(e))?;
+
                 pc + 4
             }
             Inst::Add { dest, src1, src2 } => {
-                if dest.0 != 0 {
-                    regs[dest.0 as usize] = simple_add(
-                        t,
-                        &regs[src1.0 as usize],
-                        &regs[src2.0 as usize],
-                        zero.clone(),
-                        zero.clone(),
-                        one.clone(),
-                    )
-                    .map_err(|e| ErtError::Emitted(e))?;
-                }
+                regs[dest.0 as usize] = simple_add(
+                    t,
+                    &regs[src1.0 as usize],
+                    &regs[src2.0 as usize],
+                    zero.clone(),
+                    zero.clone(),
+                    one.clone(),
+                )
+                .map_err(|e| ErtError::Emitted(e))?;
+
                 pc + 4
             }
             Inst::Sub { dest, src1, src2 } => {
-                if dest.0 != 0 {
-                    let mut a = regs[src1.0 as usize].clone();
-                    for a in &mut a[..] {
-                        *a = t
-                            .bitxor(a.clone(), one.clone())
-                            .map_err(|e| ErtError::Emitted(e))?;
-                    }
-                    regs[dest.0 as usize] = simple_add(
-                        t,
-                        &a,
-                        &regs[src2.0 as usize],
-                        one.clone(),
-                        zero.clone(),
-                        one.clone(),
-                    )
-                    .map_err(|e| ErtError::Emitted(e))?;
+                let mut a = regs[src1.0 as usize].clone();
+                for a in &mut a[..] {
+                    *a = t
+                        .bitxor(a.clone(), one.clone())
+                        .map_err(|e| ErtError::Emitted(e))?;
                 }
+                regs[dest.0 as usize] = simple_add(
+                    t,
+                    &a,
+                    &regs[src2.0 as usize],
+                    one.clone(),
+                    zero.clone(),
+                    one.clone(),
+                )
+                .map_err(|e| ErtError::Emitted(e))?;
+
                 pc + 4
             }
             Inst::And { dest, src1, src2 } => {
-                if dest.0 != 0 {
-                    for i in 0..32 {
-                        regs[dest.0 as usize][i] = t
-                            .bitand(
-                                regs[src1.0 as usize][i].clone(),
-                                regs[src2.0 as usize][i].clone(),
-                            )
-                            .map_err(|e| ErtError::Emitted(e))?;
-                    }
+                for i in 0..32 {
+                    regs[dest.0 as usize][i] = t
+                        .bitand(
+                            regs[src1.0 as usize][i].clone(),
+                            regs[src2.0 as usize][i].clone(),
+                        )
+                        .map_err(|e| ErtError::Emitted(e))?;
                 }
                 pc + 4
             }
             Inst::Or { dest, src1, src2 } => {
-                if dest.0 != 0 {
-                    for i in 0..32 {
-                        regs[dest.0 as usize][i] = t
-                            .bitor(
-                                regs[src1.0 as usize][i].clone(),
-                                regs[src2.0 as usize][i].clone(),
-                            )
-                            .map_err(|e| ErtError::Emitted(e))?;
-                    }
+                for i in 0..32 {
+                    regs[dest.0 as usize][i] = t
+                        .bitor(
+                            regs[src1.0 as usize][i].clone(),
+                            regs[src2.0 as usize][i].clone(),
+                        )
+                        .map_err(|e| ErtError::Emitted(e))?;
                 }
                 pc + 4
             }
             Inst::Xor { dest, src1, src2 } => {
-                if dest.0 != 0 {
-                    for i in 0..32 {
-                        regs[dest.0 as usize][i] = t
-                            .bitxor(
-                                regs[src1.0 as usize][i].clone(),
-                                regs[src2.0 as usize][i].clone(),
-                            )
-                            .map_err(|e| ErtError::Emitted(e))?;
-                    }
+                for i in 0..32 {
+                    regs[dest.0 as usize][i] = t
+                        .bitxor(
+                            regs[src1.0 as usize][i].clone(),
+                            regs[src2.0 as usize][i].clone(),
+                        )
+                        .map_err(|e| ErtError::Emitted(e))?;
                 }
                 pc + 4
             }
             Inst::Andi { imm, dest, src1 } => {
-                if dest.0 != 0 {
-                    for i in 0..32 {
-                        if imm.as_i32() as u32 & (1 << i) == 0 {
-                            regs[dest.0 as usize][i] = zero.clone()
-                        } else {
-                            regs[dest.0 as usize][i] = regs[src1.0 as usize][i].clone();
-                        }
+                for i in 0..32 {
+                    if imm.as_i32() as u32 & (1 << i) == 0 {
+                        regs[dest.0 as usize][i] = zero.clone()
+                    } else {
+                        regs[dest.0 as usize][i] = regs[src1.0 as usize][i].clone();
                     }
                 }
                 pc + 4
             }
             Inst::Ori { imm, dest, src1 } => {
-                if dest.0 != 0 {
-                    for i in 0..32 {
-                        if imm.as_i32() as u32 & (1 << i) != 0 {
-                            regs[dest.0 as usize][i] = one.clone()
-                        } else {
-                            regs[dest.0 as usize][i] = regs[src1.0 as usize][i].clone();
-                        }
+                for i in 0..32 {
+                    if imm.as_i32() as u32 & (1 << i) != 0 {
+                        regs[dest.0 as usize][i] = one.clone()
+                    } else {
+                        regs[dest.0 as usize][i] = regs[src1.0 as usize][i].clone();
                     }
                 }
                 pc + 4
             }
             Inst::Xori { imm, dest, src1 } => {
-                if dest.0 != 0 {
-                    for i in 0..32 {
-                        if imm.as_i32() as u32 & (1 << i) != 0 {
-                            regs[dest.0 as usize][i] = t
-                                .bitxor(regs[src1.0 as usize][i].clone(), one.clone())
-                                .map_err(|e| ErtError::Emitted(e))?;
-                        } else {
-                            regs[dest.0 as usize][i] = regs[src1.0 as usize][i].clone();
-                        }
+                for i in 0..32 {
+                    if imm.as_i32() as u32 & (1 << i) != 0 {
+                        regs[dest.0 as usize][i] = t
+                            .bitxor(regs[src1.0 as usize][i].clone(), one.clone())
+                            .map_err(|e| ErtError::Emitted(e))?;
+                    } else {
+                        regs[dest.0 as usize][i] = regs[src1.0 as usize][i].clone();
                     }
+                }
+                pc + 4
+            }
+            Inst::Slli { imm, dest, src1 } => {
+                for a in regs[dest.0 as usize].iter_mut() {
+                    *a = zero.clone()
+                }
+                for i in 0..32 {
+                    let l = imm.as_u32() & 31;
+                    if l + i >= 32 {
+                        continue;
+                    };
+                    regs[dest.0 as usize][(i + l) as usize] =
+                        regs[src1.0 as usize][i as usize].clone();
+                }
+                pc + 4
+            }
+            Inst::Srli { imm, dest, src1 } => {
+                for a in regs[dest.0 as usize].iter_mut() {
+                    *a = zero.clone()
+                }
+                for i in 0..32 {
+                    let l = imm.as_u32() & 31;
+                    if l > i {
+                        continue;
+                    };
+                    regs[dest.0 as usize][(i - l) as usize] =
+                        regs[src1.0 as usize][i as usize].clone();
+                }
+                pc + 4
+            }
+            Inst::Srai { imm, dest, src1 } => {
+                let sign = regs[dest.0 as usize][31].clone();
+                for a in regs[dest.0 as usize].iter_mut() {
+                    *a = sign.clone();
+                }
+                for i in 0..32 {
+                    let l = imm.as_u32() & 31;
+                    if l > i {
+                        continue;
+                    };
+                    regs[dest.0 as usize][(i - l) as usize] =
+                        regs[src1.0 as usize][i as usize].clone();
+                }
+                pc + 4
+            }
+
+            // memory
+            Inst::Lb { offset, dest, base } => {
+                if base != Reg::SP {
+                    return Err(ErtError::Unexpected);
+                }
+                for i in 0..32 {
+                    let j = i.min(7);
+                    regs[dest.0 as usize][i] = vstack[offset.as_u32() as usize * 8 + j].clone();
+                }
+                pc + 4
+            }
+            Inst::Lh { offset, dest, base } => {
+                if base != Reg::SP {
+                    return Err(ErtError::Unexpected);
+                }
+                for i in 0..32 {
+                    let j = i.min(15);
+                    regs[dest.0 as usize][i] = vstack[offset.as_u32() as usize * 8 + j].clone();
+                }
+                pc + 4
+            }
+            Inst::Lbu { offset, dest, base } => {
+                if base != Reg::SP {
+                    return Err(ErtError::Unexpected);
+                }
+                for i in 0..32 {
+                    let j = i.min(7);
+                    if j != i {
+                        regs[dest.0 as usize][i] = zero.clone();
+                        continue;
+                    }
+                    regs[dest.0 as usize][i] = vstack[offset.as_u32() as usize * 8 + j].clone();
+                }
+                pc + 4
+            }
+            Inst::Lhu { offset, dest, base } => {
+                if base != Reg::SP {
+                    return Err(ErtError::Unexpected);
+                }
+                for i in 0..32 {
+                    let j = i.min(15);
+                    if j != i {
+                        regs[dest.0 as usize][i] = zero.clone();
+                        continue;
+                    }
+                    regs[dest.0 as usize][i] = vstack[offset.as_u32() as usize * 8 + j].clone();
+                }
+                pc + 4
+            }
+            Inst::Lw { offset, dest, base } => {
+                if base != Reg::SP {
+                    return Err(ErtError::Unexpected);
+                }
+                for i in 0..32 {
+                    regs[dest.0 as usize][i] = vstack[offset.as_u32() as usize * 8 + i].clone();
+                }
+                pc + 4
+            }
+            Inst::Sb { offset, src, base } => {
+                if base != Reg::SP {
+                    return Err(ErtError::Unexpected);
+                }
+                for i in 0..8 {
+                    vstack[offset.as_u32() as usize * 8 + i] = regs[src.0 as usize][i].clone();
+                }
+                pc + 4
+            }
+            Inst::Sh { offset, src, base } => {
+                if base != Reg::SP {
+                    return Err(ErtError::Unexpected);
+                }
+                for i in 0..16 {
+                    vstack[offset.as_u32() as usize * 8 + i] = regs[src.0 as usize][i].clone();
+                }
+                pc + 4
+            }
+            Inst::Sw { offset, src, base } => {
+                if base != Reg::SP {
+                    return Err(ErtError::Unexpected);
+                }
+                for i in 0..32 {
+                    vstack[offset.as_u32() as usize * 8 + i] = regs[src.0 as usize][i].clone();
                 }
                 pc + 4
             }
