18d884fd9afbbb5ccd087ed449995b225033e77b
gkgoat <65402989+gkgoat1@users.noreply.github.com>
2026-08-12 01:56:35 -0400

    null context

--- crates/cirrus-core/src/lib.rs
+++ crates/cirrus-core/src/lib.rs
@@ -3,24 +3,37 @@ pub use paste::paste;
 pub trait ContextWithValue<Val> {
     type Wrapped;
 }
+impl<Val> ContextWithValue<Val> for () {
+    type Wrapped = Val;
+}
 #[macro_export]
 macro_rules! context_with_binop {
-    ($name:ident, $method:ident) => {
+    ($name:ident, $method:ident, $orig:ident) => {
         $crate::paste!{
             pub trait $name<Val>: $crate::ContextWithValue<Val>{
                 fn $method(&mut self, a: <Self as $crate::ContextWithValue<Val>>::Wrapped, b: <Self as $crate::ContextWithValue<Val>>::Wrapped) -> <Self as $crate::ContextWithValue<Val>>::Wrapped;
                 fn [<$method _assign>](&mut self, a: &mut <Self as $crate::ContextWithValue<Val>>::Wrapped, b: <Self as $crate::ContextWithValue<Val>>::Wrapped);
             }
+            const _: ()={
+                impl<Val: ::core::ops::$orig<Val, Output = Val> + ::core::ops::[<$orig Assign>]<Val>> $name<Val> for (){
+                    fn $method(&mut self, a: <Self as $crate::ContextWithValue<Val>>::Wrapped, b: <Self as $crate::ContextWithValue<Val>>::Wrapped) -> <Self as $crate::ContextWithValue<Val>>::Wrapped{
+                        ::core::ops::$orig::$method(a,b)
+                    }
+                    fn [<$method _assign>](&mut self, a: &mut <Self as $crate::ContextWithValue<Val>>::Wrapped, b: <Self as $crate::ContextWithValue<Val>>::Wrapped){
+                        ::core::ops::[<$orig Assign>]::[<$method _assign>](a,b);
+                    }
+                }
+            };
         }
     };
 }
-context_with_binop!(ContextWithAdd, add);
-context_with_binop!(ContextWithSub, sub);
-context_with_binop!(ContextWithMul, mul);
-context_with_binop!(ContextWithDiv, div);
-context_with_binop!(ContextWithAnd, and);
-context_with_binop!(ContextWithOr, or);
-context_with_binop!(ContextWithXor, xor);
+context_with_binop!(ContextWithAdd, add, Add);
+context_with_binop!(ContextWithSub, sub, Sub);
+context_with_binop!(ContextWithMul, mul, Mul);
+context_with_binop!(ContextWithDiv, div, Div);
+context_with_binop!(ContextWithBitAnd, bitand, BitAnd);
+context_with_binop!(ContextWithBitOr, bitor, BitOr);
+context_with_binop!(ContextWithBitXor, bitxor, BitXor);
 pub trait ContextWithMux<Val>: ContextWithValue<bool> + ContextWithValue<Val> {
     fn mux(
         &mut self,
@@ -28,4 +41,14 @@ pub trait ContextWithMux<Val>: ContextWithValue<bool> + ContextWithValue<Val> {
         then: <Self as ContextWithValue<Val>>::Wrapped,
         r#else: <Self as ContextWithValue<Val>>::Wrapped,
     ) -> <Self as ContextWithValue<Val>>::Wrapped;
+}
+impl<Val> ContextWithMux<Val> for () {
+    fn mux(
+        &mut self,
+        cond: <Self as ContextWithValue<bool>>::Wrapped,
+        then: <Self as ContextWithValue<Val>>::Wrapped,
+        r#else: <Self as ContextWithValue<Val>>::Wrapped,
+    ) -> <Self as ContextWithValue<Val>>::Wrapped {
+        if cond { then } else { r#else }
+    }
 }
