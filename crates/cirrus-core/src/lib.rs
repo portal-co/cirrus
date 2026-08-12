9c9feddc5fea463d85f53fcc82ea987ac6c5114b
gkgoat <65402989+gkgoat1@users.noreply.github.com>
2026-08-12 01:23:46 -0400

    rename, mux

--- crates/cirrus-core/src/lib.rs
+++ crates/cirrus-core/src/lib.rs
@@ -1,15 +1,15 @@
 #![no_std]
 pub use paste::paste;
-pub trait CirrusContextWithValue<Val> {
+pub trait ContextWithValue<Val> {
     type Wrapped;
 }
 #[macro_export]
 macro_rules! context_with_binop {
     ($name:ident, $method:ident) => {
         $crate::paste!{
-            pub trait $name<Val>: $crate::CirrusContextWithValue<Val>{
-                fn $method(&mut self, a: <Self as $crate::CirrusContextWithValue<Val>>::Wrapped, b: <Self as $crate::CirrusContextWithValue<Val>>::Wrapped) -> <Self as $crate::CirrusContextWithValue<Val>>::Wrapped;
-                fn [<$method _assign>](&mut self, a: &mut <Self as $crate::CirrusContextWithValue<Val>>::Wrapped, b: <Self as $crate::CirrusContextWithValue<Val>>::Wrapped);
+            pub trait $name<Val>: $crate::ContextWithValue<Val>{
+                fn $method(&mut self, a: <Self as $crate::ContextWithValue<Val>>::Wrapped, b: <Self as $crate::ContextWithValue<Val>>::Wrapped) -> <Self as $crate::ContextWithValue<Val>>::Wrapped;
+                fn [<$method _assign>](&mut self, a: &mut <Self as $crate::ContextWithValue<Val>>::Wrapped, b: <Self as $crate::ContextWithValue<Val>>::Wrapped);
             }
         }
     };
@@ -18,3 +18,11 @@ context_with_binop!(ContextWithAdd, add);
 context_with_binop!(ContextWithSub, sub);
 context_with_binop!(ContextWithMul, mul);
 context_with_binop!(ContextWithDiv, div);
+pub trait ContextWithMux<Val>: ContextWithValue<bool> + ContextWithValue<Val> {
+    fn mux(
+        &mut self,
+        cond: <Self as ContextWithValue<bool>>::Wrapped,
+        then: <Self as ContextWithValue<Val>>::Wrapped,
+        r#else: <Self as ContextWithValue<Val>>::Wrapped,
+    ) -> <Self as ContextWithValue<Val>>::Wrapped;
+}
