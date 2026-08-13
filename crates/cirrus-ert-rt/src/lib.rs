6a4b87dc4f776e687f11ed0ba90f761909d366d0
gkgoat <65402989+gkgoat1@users.noreply.github.com>
2026-08-12 21:05:42 -0400

    ert rt

--- crates/cirrus-ert-rt/src/lib.rs
+++ crates/cirrus-ert-rt/src/lib.rs
@@ -1,7 +1,11 @@
 #![no_std]
 use core::arch::asm;
-use core::array;
+use core::{array, iter};
+
+use sha2::Digest;
+/// Hash a value, with host-provided salts
 pub fn hash(mut v: [u8; 32]) -> [u8; 32] {
+    v = sha2::Sha256::digest(&v).0;
     #[cfg(target_arch = "riscv32")]
     {
         let [mut a, mut b, mut c, mut d, mut e, mut f, mut g, mut h] =
@@ -19,4 +23,20 @@ pub fn hash(mut v: [u8; 32]) -> [u8; 32] {
         return v;
     }
     unreachable!()
+}
+/// Sponge construction/XOF of [`hash`]
+pub fn hash_many(x: &[u8]) -> impl Iterator<Item = u8> {
+    let mut state = [0xff; 32];
+    for c in x.chunks(16) {
+        state = hash(state);
+        state[0..16].fill(0x00);
+        state[0..(c.len())].copy_from_slice(c);
+    }
+    state = hash(state);
+    return iter::from_fn(move || {
+        let ext: [u8; 16] = array::from_fn(|i| state[i]);
+        state = hash(state);
+        Some(ext)
+    })
+    .flatten();
 }
