202f7d5b51e248148bb67d69185cc68770948eb9
gkgoat <65402989+gkgoat1@users.noreply.github.com>
2026-09-19 09:32:20 -0400

    [AI] Format AArch64 selftest build script

--- crates/ert/cirrus-aarch64-ert-selftest/build.rs
+++ crates/ert/cirrus-aarch64-ert-selftest/build.rs
@@ -1,5 +1,8 @@
 fn main() {
-    println!("cargo:rustc-link-arg=-T{}", concat!(env!("CARGO_MANIFEST_DIR"), "/linker.ld"));
+    println!(
+        "cargo:rustc-link-arg=-T{}",
+        concat!(env!("CARGO_MANIFEST_DIR"), "/linker.ld")
+    );
     println!("cargo:rustc-link-arg=-nostdlib");
     println!("cargo:rustc-link-arg=-static");
     println!("cargo:rerun-if-changed=linker.ld");
